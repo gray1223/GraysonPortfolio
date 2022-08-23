8d395cfbdeaf8455cf7d9d70a0e9a964cfbd62a4
georgianinn <gbertaina@gi-bc.com>
2022-08-22 22:25:32 -0400

    changes
    changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "93a9543c90b453f1"; }
+/******/ 		__webpack_require__.h = function() { return "b12d1290ce1ecda2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ export default function Home() {
               </div>
             </a>
 
-            
+            <a></a>
 
 
 
