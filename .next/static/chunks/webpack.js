ff8cb5cbe1f5414ba3291130d496b6767066dfec
georgianinn <gbertaina@gi-bc.com>
2022-08-25 12:40:32 +0100

    updates
    updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "92d1e49d698f3cda"; }
+/******/ 		__webpack_require__.h = function() { return "2e870fd36e4e178b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
